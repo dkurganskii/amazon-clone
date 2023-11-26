f944d7a92fcd6f173da78fbedde88d340206f294
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-11-26 10:43:22 -0600

    build banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
       </Head>
-   <Header/>
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
+
     </div>
   );
 }
