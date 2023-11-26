ed1e23a1f79ee9afc1db8ccf586f6bd5fedfc765
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-11-26 10:04:48 -0600

    add items counter in basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,8 +37,9 @@ function Header() {
                     </div>
 
                     <div className='link relative flex items-center'>
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                         <ShoppingCartIcon className='h-10' />
-                        <p className='font-extrabold md:text-sm'>Basket</p>
+                        <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
                     </div>
                 </div>
             </div>
