d5ed8cda3a603b481eeeb7608b3b0991ed12617d
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-12-01 09:12:15 -0600

    add product component

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Product from './Product'
 
 function ProductFeed({ products }) {
     return (
         <div>
             <h1>Products here...</h1>
             {products.map(({id, title, description, category, image}) => (
-                <p>{title}</p>
+                <Product 
+                key={id}
+                id={id} 
+                title={title} 
+                description={description} 
+                category={category} 
+                image={image}
+                />
             ))}
         </div>
     )
