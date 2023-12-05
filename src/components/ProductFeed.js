59615286cc5c0bc243f8c3b150f49e677aebe186
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-12-05 09:18:29 -0600

    build product feed

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,19 +3,47 @@ import Product from './Product'
 
 function ProductFeed({ products }) {
     return (
-        <div>
-            <h1>Products here...</h1>
-            {products.map(({id, title, description, category, image, price}) => (
-                <Product 
-                key={id}
-                id={id} 
-                title={title} 
-                description={description} 
-                category={category} 
-                image={image}
-                price={price}
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md: -mt-52 mx-auto'>
+
+            {products.slice(0, 4).map(({ id, title, description, category, image, price }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    description={description}
+                    category={category}
+                    image={image}
+                    price={price}
                 />
             ))}
+
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
+            <div className='md:col-span-2'>
+                {products.slice(4, 5).map(({ id, title, description, category, image, price }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        description={description}
+                        category={category}
+                        image={image}
+                        price={price}
+                    />
+                ))}
+            </div>
+
+            {products.slice(5, products.length).map(({ id, title, description, category, image, price }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    description={description}
+                    category={category}
+                    image={image}
+                    price={price}
+                />
+            ))}
+
         </div>
     )
 }
